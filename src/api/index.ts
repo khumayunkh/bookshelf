ecf50bef5fdf12be334cd729833e53fa166d082e
khumayunkh <hasanovhumayun15@gmail.com>
2022-12-17 05:52:21 +0500

    api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,22 +2,21 @@ import axios from "axios";
 
 
 export const client = axios.create({
-    baseURL: 'https://splunk.backpackertrail.de/',
+    baseURL: 'http://localhost:3006/',
     timeout: 10000,
     headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+        'Key': `${localStorage.getItem('key')}`,
+        'Sign': `${localStorage.getItem('sign')}`
     }
 });
 
 
-const setConfiguration = (client) => {
+const setConfiguration = (client: any) => {
     client.interceptors.request.use(
-        (config) => {
-            config.headers && (config.headers['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`)
+        (config : any) => {
+            config.headers && (config.headers['Key'] = `${localStorage.getItem('key')}`) && (config.headers['Sign'] = `${localStorage.getItem('sign')}`)
             return config
-        },  
-        error => Promise.reject(error)
+        }, 
     )
 }
 
