d466e9b73314db6f122b861942a5bd3bcf4d660a
khumayunkh <hasanovhumayun15@gmail.com>
2022-12-19 14:19:00 +0500

    add books

--- src/components/AddBook/AddBook.tsx
+++ src/components/AddBook/AddBook.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { useAppDispatch } from "../../hook";
 import { addNewBooks } from "../../store/BooksSlice";
@@ -6,15 +7,19 @@ import style from './addbook.module.css'
 
 export const AddBook = () => {
     const dispatch = useAppDispatch()
-    const [isbn, setIsbn] = useState<string>('961439574164')
-    const [author,setAuthor] = useState<string>('meedsa')
     const {register, handleSubmit} = useForm()
-
     
     const isbnRegister = register('isbn')
+    const authorRegister = register('author')
+    const titleRegister = register('title')
+    const dateRegister = register('date')
+    const pageRegister = register('page')
     
     const onSubmit = async(data: any) => {
-        dispatch(addNewBooks({author,isbn}))
+        dispatch(addNewBooks({
+            author: data.author,
+            isbn: data.isbn
+        }))
     }
     
     return(
@@ -22,7 +27,51 @@ export const AddBook = () => {
         <div className={style.container}>
             <div className={style.add}>
                 <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-                    <input {...isbnRegister} type='text'/>
+                    <TextField
+                    sx={{ m: 1, width: '35ch' }}
+                    id="standard-multiline-flexible"
+                    label="Author of book"
+                    {...authorRegister}
+                    multiline
+                    maxRows={4}
+                    variant="standard"
+                    />
+                    <TextField
+                    sx={{ m: 1, width: '35ch' }}
+                    id="standard-multiline-flexible"
+                    label="Title of book"
+                    {...titleRegister}
+                    multiline
+                    maxRows={4}
+                    variant="standard"
+                    />
+                    <TextField
+                    sx={{ m: 1, width: '35ch' }}
+                    id="standard-multiline-flexible"
+                    label="Date of publish"
+                    {...dateRegister}
+                    multiline
+                    maxRows={4}
+                    variant="standard"
+                    />
+                    <TextField
+                    sx={{ m: 1, width: '35ch' }}
+                    id="standard-multiline-flexible"
+                    label="Pages"
+                    {...pageRegister}
+                    multiline
+                    maxRows={4}
+                    variant="standard"
+                    />
+                    <TextField
+                        sx={{ m: 1, width: '35ch' }}
+                        id="standard-multiline-flexible"
+                        label="Isbn of book"
+                        {...isbnRegister}
+                        multiline
+                        maxRows={4}
+                        variant="standard"
+                    />
                     <button className={style.btn}>Add New Book</button>
                 </form>
             </div>
